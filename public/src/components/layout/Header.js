d0eeb9292783240532faac0d0c291688aed5431d
JoshuaNeel <49606617+JoshuaNeel@users.noreply.github.com>
2021-10-02 00:12:27 +0530

    Music_Lib

---
+++ public/src/components/layout/Header.js
@@ -0,0 +1,9 @@
+// Header component
+export default () => {
+  return (
+    <div className="title">
+      <span className="caption">Simple <strong>Music App</strong>
+      </span>
+    </div>
+  );
+};
